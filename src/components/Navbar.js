5cae1d1aa0869b586a06c92f43087abc6f60b95a
saim hashmi <hashmisaim037@gmail.com>
2024-09-14 20:40:10 -0400

    Using updated react props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
-export default function Navbar(props) {
+export default function Navbar({ mode = "light", toggleMode = () => {} }) { 
   return (
     <>
-      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+      <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Text Operations</Link>
+          <motion.div>
+            <Link className="navbar-brand" to="/">Text Operations</Link>
+          </motion.div>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -20,13 +23,13 @@ export default function Navbar(props) {
                 <Link className="nav-link" to="/about">About</Link>
               </li>
             </ul>
-            <div className={`form-check form-switch text-${props.mode === "light" ? "dark" : "light"}`}>
+            <div className={`form-check form-switch text-${mode === "light" ? "dark" : "light"}`}>
               <input
                 className="form-check-input"
                 type="checkbox"
                 role="switch"
                 id="flexSwitchCheckDefault"
-                onClick={props.toggleMode} // This ensures the toggleMode function is called when clicked
+                onClick={toggleMode} // This ensures the toggleMode function is called when clicked
               />
               <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
                 Enable Dark Mode
@@ -42,8 +45,4 @@ export default function Navbar(props) {
 Navbar.propTypes = {
   mode: PropTypes.string.isRequired,
   toggleMode: PropTypes.func.isRequired,
-};
-
-Navbar.defaultProps = {
-  mode: "light",
 };
